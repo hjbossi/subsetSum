d1024dd1f745cf6fdeb892de6b512114181970b0
hjbossi <hjbossi@colby.edu>
2017-04-14 17:00:26 -0400

    Add files via upload

---
+++ ExhaustiveSearch.java
@@ -0,0 +1,32 @@
+// Hannah Bossi 
+// CS 375 Project 4 
+// ExhaustiveSearch.java
+// Solves the Subset Sum Problem using exhaustive search 
+class ExhaustiveSearch{
+
+	// stores the set of numbers
+	private int[] set; 
+	//stores the sum to search for
+	private int n; 
+	
+	
+	// takes in a set and a sum to search for
+	public ExhaustiveSearch(int[] array, int num){
+		this.set = array; 
+		this.n = num; 
+	}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+} 
